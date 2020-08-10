4e8159fe187ac2b64b93c19029430977b36bf7a9
rzc9527 <1397417028@qq.com>
2020-08-10 15:40:29 +0800

    original1

---
+++ jump7.py
@@ -0,0 +1,6 @@
+                                                                                                                                                                                     a=0
+while a<100:
+    a+=1
+    if a%7!=0 and (a-7)%10!=0 and a//10!=7:
+        print(a)
+
